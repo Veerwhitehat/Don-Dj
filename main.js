9e78ab01a8fb97986893b53957a87bebeb14d1d0
Veerwhitehat <92624033+Veerwhitehat@users.noreply.github.com>
2022-07-15 11:54:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 song1=""
+song2=""
 
+song1_status="";
+song2_status="";
+
+leftWristX=0;
+leftWristY=0;
+
+rightWristX=0;
+rightWristY=0;
 
 function preload() {
 song1=loadSound("mashup.mp3");
+song2=loadSound("ami.mp3");
 }
 
 function setup() {
@@ -11,15 +21,48 @@ function setup() {
     canvas.center();
     video= createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video, DJisReady);
+    poseNet.on("pose", gotPoses);
+}
+function DJisReady() {
+
+    console.log("DJisReady");
+    
 }
 
 function draw(){
 
     image(video,0,0,500,400);
+    song1_status= song1.isPlaying();
+    song2_status= song2.isPlaying();
     
 }
+
 function play() {
 
-    song3.play("mashup.mp3");
+    song1.play("mashup.mp3");
+   
+    song1.setVolume(1);
+    song1.rate(1);
+        
+    }
+    
+    
+
+function gotPoses(results) {
+
+    if (results.length>0) {
+
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+
+        
+
+
+        
+    }
     
 }
